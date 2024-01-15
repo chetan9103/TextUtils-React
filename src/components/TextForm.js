2c9ab84fb511481ab4937720f2908eb16c45790e
codechets <chetangupta479@gmail.com>
2024-01-15 22:16:23 +0530

    final changes-1

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -34,19 +34,19 @@ export default function TextForm(props) {
         props.showAlert("extra spaces removed","success")
 
         }
-    const[text,setText]=useState("enter the text")
+    const[text,setText]=useState("")
   return (
     <div  style={{color:props.theme==="light"?"black":"white"}}>
      <form>
   <h1>{props.heading}</h1>
   <div className="form-group">
     
-    <textarea className="form-control" value={text} onChange={handleOnchange} style={{backgroundColor:props.theme==="light"?"white":"grey",color:props.theme==="light"?"black":"white"}} id="mybox" rows="8"></textarea>
+    <textarea className="form-control" value={text}  placeholder="Enter the text"onChange={handleOnchange} style={{backgroundColor:props.theme==="light"?"white":"grey",color:props.theme==="light"?"black":"white"}} id="mybox" rows="8"></textarea>
   </div>
-  <button className="btn btn-primary mx-1 my-1" onClick={handleOnClick1} >convert to uppercase</button>
-  <button className="btn btn-primary mx-1 " onClick={handleOnClick3} >convert to lower case</button>
-  <button className="btn btn-primary mx-1" onClick={handleOnClick2} >remove extra spaces</button>
-  <button className="btn btn-danger mx-1" onClick={handleOnClick4} >Clear Text</button>
+  <button  disabled={text.length===0}className="btn btn-primary mx-1 my-1" onClick={handleOnClick1} >convert to uppercase</button>
+  <button disabled={text.length===0} className="btn btn-primary mx-1 my-1 " onClick={handleOnClick3} >convert to lower case</button>
+  <button disabled={text.split(" ").filter((word=>word==="")).length<=2} className="btn btn-primary mx-1 my-1" onClick={handleOnClick2} >remove extra spaces</button>
+  <button disabled={text.length===0} className="btn btn-danger mx-1 my-1" onClick={handleOnClick4} >Clear Text</button>
 </form>
 <div className="container my-3">
   <h2>Your text  summary</h2>
@@ -54,7 +54,7 @@ export default function TextForm(props) {
   <p>{text.split(" ").filter((word=>word!=="")).length}words and {text.length}characters</p>
   <p>{text.split(" ").filter((word=>word!=="")).length*0.008} minutes to read</p>
   <h2>Preview</h2>
-  <p>{text.length>0?text:"enter the text in the text area to preview"}</p>
+  <p>{text.length>0?text:"Nothing to preview"}</p>
 </div>
     </div>
   )
